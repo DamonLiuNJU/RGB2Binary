377f2456575f76d454580b3fd8ec9be2967b3124
DamonLiu <liu394000785@sina.com>
2015-10-29 00:41:40 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Main {
+	public void binaryImage() throws IOException {
+		File file = new File("./orig.jpg");
+		BufferedImage image = ImageIO.read(file);
+
+		int width = image.getWidth();
+		int height = image.getHeight();
+
+		BufferedImage grayImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);// 重点，技巧在这个参数BufferedImage.TYPE_BYTE_BINARY
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				int rgb = image.getRGB(i, j);
+				grayImage.setRGB(i, j, rgb);
+			}
+		}
+
+		File newFile = new File("./orig_binary.jpg");
+		ImageIO.write(grayImage, "jpg", newFile);
+	}
+
+	public void grayImage() throws IOException {
+		File file = new File("./orig.jpg");
+		BufferedImage image = ImageIO.read(file);
+
+		int width = image.getWidth();
+		int height = image.getHeight();
+
+		BufferedImage grayImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);// 重点，技巧在这个参数BufferedImage.TYPE_BYTE_GRAY
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				int rgb = image.getRGB(i, j);
+				grayImage.setRGB(i, j, rgb);
+			}
+		}
+
+		File newFile = new File("./orig_gray.jpg");
+		ImageIO.write(grayImage, "jpg", newFile);
+	}
+
+	public static void main(String[] args) throws IOException {
+		Main demo = new Main();
+		demo.binaryImage();
+		demo.grayImage();
+	}
+
+}
